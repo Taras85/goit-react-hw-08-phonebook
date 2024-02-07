646f1ea0290ff13f3e4f20dab66abe37c074f5c9
Taras <topanasenko85@gmail.com>
2024-02-07 22:04:37 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const Header = ({ showModal }) => {
   return (
-    <nav className="navbar bg-dark mb3 navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1 text-success">Navbar</span>
-        <div className="collapse navbar-collapse text-white " id="navbarNavAltMarkup">
-          <div className="navbar-nav">
+    <nav className="">
+      <div className="">
+        <span className="">Navbar</span>
+        <div className=" " id="">
+          <div className="">
             {/* <NavLink className="nav-link text-white " aria-current="page" to="/">
               Home
             </NavLink>
@@ -20,7 +20,7 @@ const Header = ({ showModal }) => {
 
           </div>
         </div>
-        <button className="btn btn-outline-success" onClick={showModal}>
+        <button className="" onClick={showModal}>
           Open Modal
         </button>
       </div>
